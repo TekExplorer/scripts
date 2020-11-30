d31d7aac2d2259f07f1a30dd93bf7891f61c4b41
Omar Kamel <30291302+TekExplorer@users.noreply.github.com>
2020-11-30 10:07:56 -0500

    Better env get
    get env much less jank

--- ptero/backup_panel.sh
+++ ptero/backup_panel.sh
@@ -2,7 +2,7 @@
 
 DEFAULT_BACKUP_DIR=/var/lib/ptero.sh/panel-backups
 BACKUP_DIR=$(echo ${1:-$DEFAULT_BACKUP_DIR} | sed -e s./$..g)
-TIME_STAMP=$(date "+%b_%d_%Y_%H_%M_%S")
+TIME_STAMP="$(date --iso-8601=s)"
 envfile=/var/www/pterodactyl/.env
 
 if [[ $EUID -ne 0 ]]; then
@@ -34,7 +34,6 @@ case "$response" in
         ;;
 esac
 
-
 error() {
   COLOR_RED='\033[0;31m'
   COLOR_NC='\033[0m'
@@ -44,29 +43,55 @@ error() {
   echo ""
 }
 
-parse_env1() {
+#################################################
+#  parse_env() {
+#    grep $1 $envfile | cut -d '=' -f2
+#  }
+#
+#  # grab variables from .env file
+#  # parse_env DB_HOST DB_DATABASE DB_PASSWORD DB_USERNAME
+#  DB_HOST=$(parse_env DB_HOST)
+#  DB_DATABASE=$(parse_env DB_DATABASE)
+#  DB_PASSWORD=$(parse_env DB_PASSWORD)
+#  DB_USERNAME=$(parse_env DB_USERNAME)
+###################################################
+
+# set -euo pipefail
+
+parse_env() {
+  eval "$(
+    source $envfile &>/dev/null # Source all variables temporarily
+    for variable in "${@}"; do
+      # -g - make variables global (out of function scope)
+      # -- stop processing flags/options
+      # "${variable}" - get content of the "variable"
+      # "${!variable}" - get value of a variable, which name the "variable" var contains (bash reflections/indirect reference)
+      # "${variable@Q}" - safely quote the content of the substitution
+      printf 'declare -g -- %s\n' "${variable}=${!variable@Q}" # Safely print quoted definitions of variables
+    done
+  )"
+
   for variable in "$@"; do
-    ${variable}=$(grep ${variable}= $envfile | cut -d '=' -f2)
+    echo "[DEBUG] got ${variable}=${!variable}"
   done
 }
 
-parse_env() {
-  grep $1 $envfile | cut -d '=' -f2
-}
+parse_env DB_HOST DB_DATABASE DB_PASSWORD DB_USERNAME
 
-# grab variables from .env file
-# parse_env DB_HOST DB_DATABASE DB_PASSWORD DB_USERNAME
-DB_HOST=$(parse_env DB_HOST)
-DB_DATABASE=$(parse_env DB_DATABASE)
-DB_PASSWORD=$(parse_env DB_PASSWORD)
-DB_USERNAME=$(parse_env DB_USERNAME)
+# Print the command we'd execute. Remove both lines with EOF to run the command instead
+cat <<EOF
+mysqldump -h $DB_HOST -u $DB_USERNAME –p"${DB_PASSWORD}" $DB_DATABASE > $BACKUP_DIR/panel-$TIME_STAMP/$DB_DATABASE.sql # Dump Panel db
+EOF
+
 
 backup_panel() {
   mkdir -p $BACKUP_DIR/panel-$TIME_STAMP
   cp $envfile $BACKUP_DIR/panel-$TIME_STAMP/.env # backup .env
   echo "* .env copied!"
   echo "* Attempting to dump database!"
-  mysqldump -h $(parse_env DB_HOST) -u $(parse_env DB_USER) -p$(parse_env DB_PASSWORD) $(parse_env DB_DATABASE) > $BACKUP_DIR/panel-$TIME_STAMP/$DB_DATABASE.sql
+ # mysqldump -h $(parse_env DB_HOST) -u $(parse_env DB_USER) -p$(parse_env DB_PASSWORD) $(parse_env DB_DATABASE) > $BACKUP_DIR/panel-$TIME_STAMP/$DB_DATABASE.sql
+  mysqldump -h $DB_HOST -u $DB_USERNAME –p"${DB_PASSWORD}" $DB_DATABASE > $BACKUP_DIR/panel-$TIME_STAMP/$DB_DATABASE.sql # Dump Panel db
+  
   echo "* Database dumped to $DB_DATABASE.sql and copied!"
   
   echo "* Archiving Database and .env"
